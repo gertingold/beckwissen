ef727826574f67c8a309915e1674af043327bbcf
Gert-Ludwig Ingold <gert.ingold@physik.uni-augsburg.de>
2025-03-06 14:07:44 +0100

    figure redesigned

--- figures/polfilter.py
+++ figures/polfilter.py
@@ -1,4 +1,3 @@
-from math import sqrt
 from pyx import canvas, color, deco, path, style, text, trafo, unit
 
 unit.set(wscale=1.3, xscale=0.85)
@@ -21,49 +20,52 @@ c.stroke(path.line(0, -0.23, 0, 0.27),
          [trafo.translate(x, y), deco.earrow.Small, style.linewidth(style._defaultlinewidth*0.9)])
 c.fill(path.circle(0, 0, r), [circletrafo, trafo.translate(1.5, 1.5), circlegrey])
 c.stroke(path.line(1.5, 1.5-r, 1.5, 1.5+r), [deco.earrow, style.linewidth.thick, color.grey(1)])
-p = path.line(0.2, 1, 1.5, 1.5)
+p = path.line(-0.45, 0.75, 1.5, 1.5)
 c.stroke(p)
 x, y = p.at(p.arclentoparam(0.2))
 c.stroke(path.line(x, y-0.23, x, y+0.27),
          [deco.earrow.Small, style.linewidth(style._defaultlinewidth*0.9)])
-c.text(0.2, 2.2, r'a')
+c.text(-0.45, 1.9, r'a')
 
-p = path.line(4.5, 1.5, 5.8, 2.0)
-c.stroke(p, [deco.earrow.small, raygrey])
-x, y = p.at(p.arclentoparam(p.arclen()-0.4))
-c.stroke(path.line(0, -0.23, 0, 0.27),
-         [trafo.rotate(-45).translated(x, y), raygrey, deco.earrow.Small,
-          style.linewidth(style._defaultlinewidth*0.9)])
-c.fill(path.circle(0, 0, r), [circletrafo, trafo.translate(4.5, 1.5), circlegrey])
-r05 = r*sqrt(0.5)
-c.stroke(path.line(-r05, -r05, r05, r05),
-         [deco.earrow, style.linewidth.thick, color.grey(1), circletrafo, trafo.translate(4.5, 1.5)])
-p = path.line(3.2, 1, 4.5, 1.5)
+xshift = 3.6
+c.fill(path.circle(0, 0, r), [circletrafo, trafo.translate(xshift+1.5, 1.5), circlegrey])
+c.stroke(path.line(0, -r, 0, r),
+         [deco.earrow, style.linewidth.thick, color.grey(1), circletrafo, trafo.translate(xshift+1.5, 1.5)])
+p = path.line(xshift-0.45, 0.75, xshift+1.5, 1.5)
 c.stroke(p)
 x, y = p.at(p.arclentoparam(0.2))
-c.stroke(path.line(x, y-0.23, x, y+0.27),
+c.stroke(path.line(x-0.23, y, x+0.27, y),
          [deco.earrow.Small, style.linewidth(style._defaultlinewidth*0.9)])
-p = path.line(4.5, 1.5, 3.6, 1.0)
-c.stroke(p, [deco.earrow.small, raygrey])
-x, y = p.at(p.arclentoparam(p.arclen()-0.4))
-c.stroke(path.line(0, -0.23, 0, 0.27),
-         [trafo.rotate(-135).translated(x, y), raygrey, deco.earrow.Small,
-          style.linewidth(style._defaultlinewidth*0.9)])
-c.text(3.2, 2.2, r'b')
-
-c.fill(path.circle(0, 0, r), [circletrafo, trafo.translate(7.5, 1.5), circlegrey])
-c.stroke(path.line(-r, 0, r, 0),
-         [deco.earrow, style.linewidth.thick, color.grey(1), circletrafo, trafo.translate(7.5, 1.5)])
-p = path.line(6.2, 1, 7.5, 1.5)
-c.stroke(p)
-x, y = p.at(p.arclentoparam(0.2))
-c.stroke(path.line(x, y-0.23, x, y+0.27),
-         [deco.earrow.Small, style.linewidth(style._defaultlinewidth*0.9)])
-p = path.line(7.5, 1.5, 6.6, 1.0)
+p = path.line(xshift+1.5, 1.5, xshift+0.24, 0.8)
 c.stroke(p, [deco.earrow.small])
+x, y = p.at(p.arclentoparam(p.arclen()-0.3))
+c.stroke(path.line(-0.23, 0, 0.27, 0),
+         [trafo.translate(x, y), deco.earrow.Small, style.linewidth(style._defaultlinewidth*0.9)])
+c.text(xshift-0.45, 1.9, r'b')
+
+xshift = 1.4
+yshift = -2.0
+p = path.line(xshift+1.5, yshift+1.5, xshift+2.8, yshift+2.0)
+c.stroke(p, [deco.earrow.small, raygrey])
 x, y = p.at(p.arclentoparam(p.arclen()-0.4))
 c.stroke(path.line(0, -0.23, 0, 0.27),
-         [trafo.translate(x, y), deco.earrow.Small, style.linewidth(style._defaultlinewidth*0.9)])
-c.text(6.2, 2.2, r'c')
+         [trafo.translate(x, y), raygrey, deco.earrow.Small,
+          style.linewidth(style._defaultlinewidth*0.9)])
+c.fill(path.circle(0, 0, r), [circletrafo, trafo.translate(xshift+1.5, yshift+1.5), circlegrey])
+c.stroke(path.line(0, -r, 0, r),
+         [deco.earrow, style.linewidth.thick, color.grey(1), circletrafo,
+          trafo.translate(xshift+1.5, yshift+1.5)])
+p = path.line(xshift-0.45, yshift+0.75, xshift+1.5, yshift+1.5)
+c.stroke(p)
+x, y = p.at(p.arclentoparam(0.2))
+c.stroke(path.line(0, -0.23, 0, 0.27),
+         [trafo.rotate(-45).translated(x, y), deco.earrow.Small, style.linewidth(style._defaultlinewidth*0.9)])
+p = path.line(xshift+1.5, yshift+1.5, xshift+0.24, yshift+0.8)
+c.stroke(p, [deco.earrow.small, raygrey])
+x, y = p.at(p.arclentoparam(p.arclen()-0.3))
+c.stroke(path.line(0, -0.23, 0, 0.27),
+         [trafo.rotate(-90).translated(x, y), raygrey, deco.earrow.Small,
+          style.linewidth(style._defaultlinewidth*0.9)])
+c.text(xshift-0.45, yshift+1.9, r'c')
 
 c.writePDFfile()
