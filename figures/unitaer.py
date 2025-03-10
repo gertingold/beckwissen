6063be2bb62e4e0ee49922272a3d714931823b02
Gert-Ludwig Ingold <gert.ingold@physik.uni-augsburg.de>
2025-03-10 15:01:38 +0100

    Deutsch algorithm added

--- figures/unitaer.py
+++ figures/unitaer.py
@@ -25,4 +25,25 @@ c.text(xoff-1.1, 0, 'x', [text.halign.right, text.valign.middle])
 c.text(xoff+1.1, 0, 'f(x)', [text.valign.middle])
 c.fill(path.rect(xoff-0.5, -0.5, 1, 1), [color.grey(1), deco.stroked([color.grey(0)])])
 c.text(xoff, 0, '\Large f', [text.halign.center, text.valign.middle])
+
+xoff =-2.7 
+yoff = -3
+for y in (-0.7, 0.7):
+    c.stroke(path.line(xoff-2.5, yoff+y, xoff+2.5, yoff+y))
+    c.fill(path.rect(xoff-2.1, yoff+y-0.3, 0.6, 0.6), [color.grey(1), deco.stroked([color.grey(0)])])
+    c.text(xoff-1.8, yoff+y, 'H', [text.halign.center, text.valign.middle])
+c.text(xoff-2.6, yoff+0.7, '|0⟩', [text.halign.right, text.valign.middle])
+c.text(xoff-2.6, yoff-0.7, '|1⟩', [text.halign.right, text.valign.middle])
+c.fill(path.rect(xoff-1, yoff-1, 2, 2), [color.grey(1), deco.stroked([color.grey(0)])]) 
+c.fill(path.rect(xoff+1.5, yoff+0.7-0.3, 0.6, 0.6), [color.grey(1), deco.stroked([color.grey(0)])])
+c.stroke(path.path(path.moveto(xoff+2.5, yoff+0.7-0.25),
+                   path.lineto(xoff+2.5, yoff+0.7+0.25),
+                   path.arcn(xoff+2.5, yoff+0.7, 0.25, 90, 270),
+                   path.closepath()), [deco.filled([color.grey(0.7)])])
+c.text(xoff+2.9, yoff+0.8, '\small |0⟩ → konstant')
+c.text(xoff+2.9, yoff+0.6, '\small |1⟩ → ausgeglichen', [text.valign.top])
+c.text(xoff+1.8, yoff+0.7, 'H', [text.halign.center, text.valign.middle])
+c.text(xoff, yoff, r'\Large U$_\text{f}$', [text.halign.center, text.valign.middle])
+c.text(xoff-3.5, yoff+0.9, '\large c')
+
 c.writePDFfile()
