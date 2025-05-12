ec854ed7a4f5ce05cee2c25c440af6d0a66f09e0
Gert-Ludwig Ingold <gert.ingold@physik.uni-augsburg.de>
2025-05-12 14:10:22 +0200

    some fontsizes decreased
    as requested by publisher on May 12, 2025

--- figures/unitaer.py
+++ figures/unitaer.py
@@ -11,20 +11,20 @@ c = canvas.canvas()
 for y in (-0.7, 0.7):
     c.stroke(path.line(-1.5, y, 1.5, y))
 c.fill(path.rect(-1, -1, 2, 2), [deco.stroked([color.grey(0)]), color.grey(1)])
-c.text(0, 0, r'\Large U$_\text{f}$', [text.halign.center, text.valign.middle])
+c.text(0, 0, r'U$_\text{f}$', [text.halign.center, text.valign.middle])
 c.text(-1.6, 0.7, 'x', [text.halign.right, text.valign.middle])
 c.text(-1.6, -0.7, 'y', [text.halign.right, text.valign.middle])
 c.text(1.6, 0.7, 'x', [text.valign.middle])
 c.text(1.6, -0.7, 'y$\oplus$f(x)', [text.valign.middle])
-c.text(-2.2, 0.8, '\large b')
+c.text(-2.2, 0.8, 'b')
 
 xoff = -5
-c.text(xoff-1.2, 0.8, '\large a')
+c.text(xoff-1.2, 0.8, 'a')
 c.stroke(path.line(xoff-1, 0, xoff+1, 0))
 c.text(xoff-1.1, 0, 'x', [text.halign.right, text.valign.middle])
 c.text(xoff+1.1, 0, 'f(x)', [text.valign.middle])
 c.fill(path.rect(xoff-0.5, -0.5, 1, 1), [color.grey(1), deco.stroked([color.grey(0)])])
-c.text(xoff, 0, '\Large f', [text.halign.center, text.valign.middle])
+c.text(xoff, 0, 'f', [text.halign.center, text.valign.middle])
 
 xoff =-2.7 
 yoff = -3
@@ -41,7 +41,7 @@ c.stroke(path.path(path.arc(xoff+2.5, yoff+0.7, 0.3, -55, 55)))
 c.text(xoff+2.9, yoff+0.8, '|0⟩ → konstant')
 c.text(xoff+2.9, yoff+0.6, '|1⟩ → ausgeglichen', [text.valign.top])
 c.text(xoff+1.8, yoff+0.7, 'H', [text.halign.center, text.valign.middle])
-c.text(xoff, yoff, r'\Large U$_\text{f}$', [text.halign.center, text.valign.middle])
-c.text(xoff-3.5, yoff+0.9, '\large c')
+c.text(xoff, yoff, r'U$_\text{f}$', [text.halign.center, text.valign.middle])
+c.text(xoff-3.5, yoff+0.9, 'c')
 
 c.writePDFfile()
